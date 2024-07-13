8c78676f7414242276470ae79949a3ea406de49b
H3L70N <heltonnguammbe@gmail.com>
2024-07-13 18:40:00 +0200

    maps

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
-	count(10)
+	menu := map[string]float64{
+		"Soup":    4.99,
+		"Pie":     7.99,
+		"Salad":   3.99,
+		"pudding": 3.55,
+	}
+
+	fmt.Println(menu, menu["pudding"])
+
+	for k, v := range menu {
+		fmt.Println(k, "-", v)
+	}
 }
