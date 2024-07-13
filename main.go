65e99190ec78d55c345f5fec23a32e30bacc1667
H3L70N <heltonnguammbe@gmail.com>
2024-07-13 18:29:56 +0200

    package scope

--- main.go
+++ main.go
@@ -1,25 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
-func getInitials(n string) (string, string) {
-	names := strings.Split(strings.ToUpper(n), " ")
-	var initials []string
-	for _, v := range names {
-		initials  = append(initials, v[:1])
-	}
-	if len(initials) > 1 {
-		return initials[0], initials[1]
-	} else if len(initials) == 0 {
-		return "", ""
-	} else {
-		return initials[0], ""
-	}
-}
-
 func main() {
-	fmt.Println(getInitials("helton guambe"))
+	count(10)
 }
