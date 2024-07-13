c32f887b67e635e8242cb0f5a442e3bc25f4b9d5
H3L70N <heltonnguammbe@gmail.com>
2024-07-13 18:15:02 +0200

    multiple return values

--- main.go
+++ main.go
@@ -2,37 +2,24 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"strings"
 )
 
-func sayGreetings(n string) {
-	fmt.Printf("Hello %v \n", n)
-}
-
-func sayBye(n string) {
-	fmt.Printf("Goodbye %v \n", n)
-}
-
-func cicleFunction(n []string, f func(string)) {
-	for _, v := range n {
-		f(v)
+func getInitials(n string) (string, string) {
+	names := strings.Split(strings.ToUpper(n), " ")
+	var initials []string
+	for _, v := range names {
+		initials  = append(initials, v[:1])
+	}
+	if len(initials) > 1 {
+		return initials[0], initials[1]
+	} else if len(initials) == 0 {
+		return "", ""
+	} else {
+		return initials[0], ""
 	}
 }
 
-func circleArea(r float64) float64{
-	return math.Pi * math.Pow(r, 2)
-}
-
 func main() {
-	nameOne := "Helton"
-	nameTwo := "Alceu"
-	nameThree := "Hidilson"
-
-	names := []string{nameOne, nameTwo, nameThree}
-
-	sayGreetings(nameOne)
-	sayBye(nameTwo)
-	cicleFunction(names, sayGreetings)
-	cicleFunction(names, sayBye)
-	fmt.Printf("%0.2f \n",circleArea(math.Sqrt(10)))
+	fmt.Println(getInitials("helton guambe"))
 }
